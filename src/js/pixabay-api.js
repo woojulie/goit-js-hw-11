12946598521a3248022d2edc25d31d26f26d5e62
Julie Pazushkina <162894528+woojulie@users.noreply.github.com>
2024-09-02 18:25:05 +0300

    new

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,25 +1,3 @@
-// const API_KEY = '45718127-30771d56f6b088ceef33bd7c7';
-// const BASE_URL = 'https://pixabay.com';
-
-// export const fetchRequest = (value) => {
-// const urlOptions = new URLSearchParams({
-//     key: API_KEY,
-//     q: value,
-//     image_type: 'photo',
-//     orientation: 'horizontal',
-//     safesearch: true,
-// });
-    
-//     console.log(`${baseURL}/api/?${urlOptions}`);
-//    return fetch(`${baseURL}/api/?${urlOptions}`)
-//     .then((resolve) => {
-//         if (!resolve.ok) {
-//             throw new Error("Error!")
-//         }
-//         return resolve.json();
-//     })
-// }
-
 export async function fetchImages(query, page = 1) {
     const API_KEY = '45718127-30771d56f6b088ceef33bd7c7';
     const BASE_URL = 'https://pixabay.com/api/';
